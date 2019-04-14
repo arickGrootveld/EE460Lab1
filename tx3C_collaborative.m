69ec2b3cef11979fd063a063785fe19fa735681f
FSKorBust <49453591+FSKorBust@users.noreply.github.com>
2019-04-13 18:03:07 -0700

    Add files via upload
    Final Commit

--- tx3C_collaborative.m
+++ tx3C_collaborative.m
@@ -1,3 +1,32 @@
 function output = tx3C_collaborative(bits)
-    output = CDMATransmitter(bits.');
+    % Collaberative CDMA Transmitter that all groups will use
+
+    %%
+    % Changeable parameters
+    Fs = 44100;
+    carrierFreq = 20;
+    upSampleRate = 100;
+    % Each team should input their own CDMA vector here
+    CDMAVector = [2 -2 1];  
+    %% Main code
+
+    symbols = bits2PAM(bits);
+
+    CDMASymbols = symbols2CDMA(symbols, CDMAVector);
+    lengthSymbols = length(CDMASymbols);
+
+    upSampledSignal = zeros(1,lengthSymbols*upSampleRate);
+    upSampledSignal(1:upSampleRate:lengthSymbols*upSampleRate) = CDMASymbols;
+    pulse = hamming(upSampleRate);
+
+    baseBandSignal = filter(pulse,1,upSampledSignal);
+    t = 1/upSampleRate:1/upSampleRate:length(baseBandSignal)/upSampleRate;
+
+    carrierSignal = cos(2*pi*carrierFreq * t);
+
+    output = carrierSignal .* baseBandSignal;
+    
+    output = [output zeros(1, 441000 - length(output))];
+    
+    output = output.';
 end
