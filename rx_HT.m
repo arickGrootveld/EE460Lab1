ab9bf515ac99ed8e825092c4a8fec2232b8062cd
Sulfurixx <36091214+Sulfurixx@users.noreply.github.com>
2019-04-16 14:02:33 -0700

    Add files via upload

--- rx_HT.m
+++ rx_HT.m
@@ -1,31 +1,29 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% GIVEN PARAMETERS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% GIVEN PARAMETERS
+% Changeable values
 bitsPerTeam = 1000;
-Fs = 44100;
 t_total = 10;
+% Constants
+Fs = 44100;
 numSamples = t_total*Fs;
 fc = 5000;              % Carrier frequency
 M = floor(numSamples/((bitsPerTeam/2)*3));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% CORRELATE received signal with pulse shape
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pulse = hamming(M);
-x1 = 2*filter((fliplr(pulse)/pow(pulse)*M), 1, y);
-plotspec(x1,1/Fs)
+%% DEMODULATION
+t = 1/Fs:1/Fs:length(y)/Fs;
+carrier = cos(2*fc*pi*t); 
+demod = y'.*carrier;          % Demod received signal
+figure(1)
+plotspec(demod,1/Fs)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% DEMODULATION
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t = 1/Fs:1/Fs:10;
-demod = cos(2*fc*pi*t); 
-x2 = y'.*demod;          % Demod received signal
-%plotspec(x1,1/Fs)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% LPF demodulated signal, x2
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LPF demodulated signal, x2
 N = 50;
 lpf = firpm(N, [0, 2100, 4410, Fs/2]/(Fs/2), [1 1 0 0]);
-x3 = 2*filter(lpf, 1, x2);
-%plotspec(x2, 1/Fs)
+x2 = 2*filter(lpf, 1, demod);
+figure(2)
+plotspec(x2, 1/Fs)
+
+%% CORRELATE received signal with pulse shape
+pulse = hamming(M);
+x1 = 2*filter((fliplr(pulse)/pow(pulse)*M), 1, y);
+%plotspec(x1,1/Fs)
+
